56027a4410e5dd1611bb90fad5e4da35a995a322
Herberthh <juracanherberth@gmail.com>
2023-06-17 02:51:20 -0600

    Nuevos cambios

--- src/components/Registrarse.js
+++ src/components/Registrarse.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Registrarse.scss";
 
 function RegistrationForm() {
@@ -9,6 +10,8 @@ function RegistrationForm() {
     description: "",
     phoneNumber: "",
   });
+
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,63 +30,73 @@ function RegistrationForm() {
       description: "",
       phoneNumber: "",
     });
+    // Mostrar el mensaje de registro exitoso
+    setRegistrationSuccess(true);
   };
 
   return (
     <div className="registration-form">
       <h2>Registro de Emprendimiento</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Nombre de Emprendimiento:</label>
-          <input
-            type="text"
-            name="businessName"
-            value={formData.businessName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Productos:</label>
-          <input
-            type="text"
-            name="products"
-            value={formData.products}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Descripción de Emprendimiento:</label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-        <div>
-          <label>Número de Teléfono:</label>
-          <input
-            type="tel"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Registrarse</button>
-      </form>
+      {registrationSuccess ? (
+        <>
+          <p className="success-message">¡Registro exitoso!</p>
+          <Link to="/">Volver a la página de inicio</Link>
+        </>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Nombre de Emprendimiento:</label>
+            <input
+              type="text"
+              name="businessName"
+              value={formData.businessName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Productos:</label>
+            <input
+              type="text"
+              name="products"
+              value={formData.products}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Descripción de Emprendimiento:</label>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+          <div>
+            <label>Número de Teléfono:</label>
+            <input
+              type="tel"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Registrarse</button>
+          <Link to="/">Cancelar</Link>
+        </form>
+      )}
     </div>
   );
 }

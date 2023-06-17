5eb5383c837cd188c6807ace6c62e0879df6d73b
Herberthh <juracanherberth@gmail.com>
2023-06-17 02:18:34 -0600

    Nuevos cambios

--- src/components/Registrarse.js
+++ src/components/Registrarse.js
@@ -1,12 +1,91 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Registrarse.scss";
 
-const Registrarse = () => {
+function RegistrationForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    businessName: "",
+    products: "",
+    description: "",
+    phoneNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar la lógica para enviar los datos de registro a tu servidor
+    console.log(formData);
+    // Restablecer el formulario
+    setFormData({
+      email: "",
+      businessName: "",
+      products: "",
+      description: "",
+      phoneNumber: "",
+    });
+  };
+
   return (
-    <div>
-      <h1>Página de Registro</h1>
-      {/* Aquí puedes agregar el contenido que deseas mostrar en la página de Registro */}
+    <div className="registration-form">
+      <h2>Registro de Emprendimiento</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Nombre de Emprendimiento:</label>
+          <input
+            type="text"
+            name="businessName"
+            value={formData.businessName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Productos:</label>
+          <input
+            type="text"
+            name="products"
+            value={formData.products}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Descripción de Emprendimiento:</label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <div>
+          <label>Número de Teléfono:</label>
+          <input
+            type="tel"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Registrarse</button>
+      </form>
     </div>
   );
-};
+}
 
-export default Registrarse;
+export default RegistrationForm;
